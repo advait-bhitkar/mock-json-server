bf9d951bfe2513105e740bd2836c13278892af22
Advait Bhitkar <bhitkaradvait@gmail.com>
2025-02-14 15:58:17 +0530

    u

--- api/server.js
+++ api/server.js
@@ -60,9 +60,9 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id',
-    '/api/vi/homepage/get-data': '/delivery-widget-data',
+    // '/api/*': '/$1',
+    // '/blog/:resource/:id/show': '/:resource/:id',
+    '/api/v1/homepage/get-data': '/delivery-widget-data',
 }))
 server.use(router)
 server.listen(3000, () => {
